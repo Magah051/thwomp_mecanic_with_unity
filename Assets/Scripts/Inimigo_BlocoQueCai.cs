bac0d5869afc5f4e16b88b0212036e5bde9f3ab0
José Magalhães <joses.magalhaes@hotmail.com>
2023-08-24 14:34:34 -0300

    Criando variáveis necessárias

--- Assets/Scripts/Inimigo_BlocoQueCai.cs
+++ Assets/Scripts/Inimigo_BlocoQueCai.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class Inimigo_BlocoQueCai : MonoBehaviour
 {
+
+    private Rigidbody2D oRigidbody2D;
+
+    [SerializeField] private float forcaDaGravidade;
+    
+    [SerializeField] private float velocidadeDeSubida;
+
+    [SerializeField] private float tempoMaximoParaSubir;
+
+    private float tempoAtualParSubir;
+    
+    private Vector3 posicaoInicial;
+
+    private bool podeCair;
+    private bool caiu;
+
+    private void Awake()
+    {
+        oRigidbody2D = GetComponent<Rigidbody2D>();
+
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
