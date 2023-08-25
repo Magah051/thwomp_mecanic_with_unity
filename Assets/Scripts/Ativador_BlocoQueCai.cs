5592a84d3b3c40013db0b41147eacabefd8569fb
José Magalhães <joses.magalhaes@hotmail.com>
2023-08-25 13:23:48 -0300

    Implementação do ativador do inimigo

---
+++ Assets/Scripts/Ativador_BlocoQueCai.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ativador_BlocoQueCai : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            transform.root.GetComponent<Inimigo_BlocoQueCai>().AtivarGravidade();
+
+        }
+    }
+}
